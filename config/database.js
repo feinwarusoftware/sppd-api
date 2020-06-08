28d28486436ad95fdff2528471f42efdfc1ca9be
Lukasz Niezabitowski <lukasznie1320@gmail.com>
2020-06-08 13:25:52 +0100

    will it deploy?

--- config/database.js
+++ config/database.js
@@ -1,17 +1,26 @@
 const path = require("path");
+const fs = require("fs");
+
+const {
+  MONGO_HOST,
+  MONGO_DB,
+  MONGO_CERT,
+} = process.env;
+
+fs.writeFileSync(path.join(__dirname, "..", "mongo_cert.pem"), MONGO_CERT);
 
 module.exports = () => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "mongoose",
-      uri: "mongodb://localhost:27000/",
+      uri: `mongodb://${MONGO_HOST}/`,
       options: {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        dbName: "rawrxd",
+        dbName: MONGO_DB,
         tls: true,
-        tlsCertificateKeyFile: path.join(__dirname, "..", "x509-full.pem"),
+        tlsCertificateKeyFile: path.join(__dirname, "..", "mongo_cert.pem"),
         authMechanism: "MONGODB-X509",
         authSource: "$external",
         tlsAllowInvalidCertificates: true,
